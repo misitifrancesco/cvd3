dff1f98eac7b7da99f2a4174602cc9492e72a5e8
gianluca politano <gian1193@gmail.com>
2020-07-31 15:09:03 +0200

    inserimento attivita

--- application/controllers/Attivita.php
+++ application/controllers/Attivita.php
@@ -16,11 +16,18 @@ class Attivita extends CI_Controller {
 
     public function admin() {
 
+        $this->dati_utente = $this->session->userdata('cvd_logged_in');
         $crud = new grocery_CRUD();
         $crud->set_theme('datatables');
         $crud->set_table('t_attivita');
+        $crud->columns('data_att', 'descrizione');
+        $crud->add_fields('data_att', 'descrizione', 'id_gruppo', 'id_fascia');
         $crud->display_as('data_att', 'Data attività');
         //$crud->where('id_gruppo="5"');
+
+        $crud->field_type('id_gruppo', 'hidden', $this->dati_utente['id_gruppo']);
+        $crud->field_type('id_fascia', 'hidden', $this->dati_utente['id_fascia']);
+
         $output = $crud->render();
 
 
